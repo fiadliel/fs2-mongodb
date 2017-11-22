8f0f2b302fcc267f1f497d37698fc5dec48bcdb3
Gary Coady <gary@lyranthe.org>
2017-11-22 16:57:39 +0000

    Add a Mongo Client creator that takes MongoClientSettings.

--- src/main/scala/org/lyranthe/fs2_mongodb/MongoClient.scala
+++ src/main/scala/org/lyranthe/fs2_mongodb/MongoClient.scala
@@ -1,12 +1,19 @@
 package org.lyranthe.fs2_mongodb
 
-import com.mongodb.async.client.{MongoClient, MongoClients}
+import com.mongodb.async.client.{MongoClient, MongoClientSettings, MongoClients}
 import fs2._
 import cats.effect.Sync
 
 object Mongo {
-  def client[F[_]](url: String)(implicit F: Sync[F]): Stream[F, MongoClient] = {
+  def fromUrl[F[_]](url: String)(implicit F: Sync[F]): Stream[F, MongoClient] = {
     Stream.bracket(F.delay(MongoClients.create(url)))(Stream.emit(_), { client =>
+      F.delay(client.close())
+    })
+  }
+
+  def fromSettings[F[_]](settings: MongoClientSettings)(
+      implicit F: Sync[F]): Stream[F, MongoClient] = {
+    Stream.bracket(F.delay(MongoClients.create(settings)))(Stream.emit(_), { client =>
       F.delay(client.close())
     })
   }
