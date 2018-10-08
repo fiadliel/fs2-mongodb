ea4e4562e1a9bc8d1170a722b83c822a9a8c0cf4
Alessandro Marrella <alessandro.marrella@hbc.com>
2018-10-08 13:38:01 +0100

    Replaced Stream.bracket with Resource.make

--- src/main/scala/org/lyranthe/fs2_mongodb/MongoClient.scala
+++ src/main/scala/org/lyranthe/fs2_mongodb/MongoClient.scala
@@ -2,19 +2,17 @@ package org.lyranthe.fs2_mongodb
 
 import com.mongodb.async.client.{MongoClient, MongoClients}
 import com.mongodb.MongoClientSettings
-
-import fs2._
-import cats.effect.Sync
+import cats.effect.{Resource, Sync}
 
 object Mongo {
-  def fromUrl[F[_]](url: String)(implicit F: Sync[F]): Stream[F, MongoClient] = {
-    Stream.bracket(F.delay(MongoClients.create(url))){ client =>
+  def fromUrl[F[_]](url: String)(implicit F: Sync[F]): Resource[F, MongoClient] =
+    Resource.make(F.delay(MongoClients.create(url))){ client =>
       F.delay(client.close())
     }
-  }
+
   def fromSettings[F[_]](settings: MongoClientSettings)(
-    implicit F: Sync[F]): Stream[F, MongoClient] = {
-    Stream.bracket(F.delay(MongoClients.create(settings)))(client =>
+    implicit F: Sync[F]): Resource[F, MongoClient] = {
+    Resource.make(F.delay(MongoClients.create(settings)))(client =>
       F.delay(client.close()))
   }
 }
